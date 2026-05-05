135f236cc86e0ca81fa6fb0a336b3b207e72b818
Liam Minopulos <Liam.Minopulos+sbsw@sportsbook-sw.com>
2026-05-05 18:56:24 +0200

    Initial commit

---
+++ src/components/header/header.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/logo.png";
+import styles from './header.module.css';
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.title}>
+        <img src={logo} className={styles.logo} alt="logo" />
+        <h1>FleetVault</h1>
+      </div>
+      <div className={styles.galaxy}>
+        Galaxy 1
+      </div>
+      <div className={styles.login}>
+        
+      </div>
+    </header>
+  );
+}
